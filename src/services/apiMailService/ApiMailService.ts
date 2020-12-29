d98fc1645917fed6ab20c29a2d3c18d2338ea00c
ThaSMorato <tha.s.morato@gmail.com>
2020-12-29 11:25:54 -0300

    env file and api_url

--- src/services/apiMailService/ApiMailService.ts
+++ src/services/apiMailService/ApiMailService.ts
@@ -1,15 +1,16 @@
 import axios from 'axios'
+import env from '../../../env'
 
 const MailApi = axios.create({
-    baseURL: 'http://192.168.18.24:3333/api/Email/SortAndSend'
+    baseURL: `${env.API_MAIL_URL}api/Email/SortAndSend`
 })
 
 export default {
     async sortAndSend(body: Array<{id: string, name: string, email: string}>){
         return new Promise((resolve, reject) => {
-            MailApi.post<any>('', {
-                data : {participants : body}
-            }).then(
+            MailApi.post<any>('', 
+                {participants : body}
+            ).then(
                 res => resolve(res)
             ).catch(
                 err => reject(err)
